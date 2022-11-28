a9ec6109526f42c0bbd5af668c34864112775baf
Kifor Kostiantyn <k.kifor@tenantcloud.com>
2022-11-28 23:30:42 +0200

    Added editable content value accessor

--- projects/forms-playground/src/app/playgrounds/custom-rating-picker/rating-picker-page/rating-picker-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/custom-rating-picker/rating-picker-page/rating-picker-page.component.ts
@@ -1,22 +1,35 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import '@polymer/paper-input/paper-textarea';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { EditableContentValueAccessor } from '../value-accessor/editable-content.directive';
 
 @Component({
   selector: 'app-rating-picker-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, EditableContentValueAccessor],
   templateUrl: './rating-picker-page.component.html',
   styleUrls: [
     '../../common-page.scss',
     './rating-picker-page.component.scss',
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RatingPickerPageComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    reviewText: ''
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
   }
 
 }
